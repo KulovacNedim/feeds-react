5d324daf1cf56812da878cfba2b2d34374131123
Nedim Kulovac <nedim.kulovac@gmail.com>
2020-04-08 12:02:19 +0200

    base url defined in .env file to interact with rest deployed at heroku

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -23,7 +23,7 @@ class Feed extends Component {
   };
 
   componentDidMount() {
-    fetch('http://localhost:8080/auth/status', {
+    fetch(process.env.REACT_APP_API_URL + '/auth/status', {
       headers: {
         Authorization: 'Bearer ' + this.props.token
       }
@@ -40,7 +40,7 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
-    const socket = openSocket('http://localhost:8080');
+    const socket = openSocket(process.env.REACT_APP_API_URL);
     socket.on('posts', data => {
       if (data.action === 'create') {
         this.addPost(data.post);
@@ -81,7 +81,7 @@ class Feed extends Component {
     });
   };
 
-
+  
   loadPosts = direction => {
     if (direction) {
       this.setState({ postsLoading: true, posts: [] });
@@ -95,7 +95,7 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8080/feed/posts?page=' + page, {
+    fetch(process.env.REACT_APP_API_URL + '/feed/posts?page=' + page, {
       headers: {
         'Authorization': 'Bearer ' + this.props.token
       }
@@ -124,7 +124,7 @@ class Feed extends Component {
   
   statusUpdateHandler = event => {
     event.preventDefault();
-    fetch('http://localhost:8080/auth/status', {
+    fetch(process.env.REACT_APP_API_URL + '/auth/status', {
       method: 'PATCH',
       headers: {
         Authorization: 'Bearer ' + this.props.token,
@@ -173,10 +173,10 @@ class Feed extends Component {
     formData.append('title', postData.title);
     formData.append('content', postData.content);
     formData.append('image', postData.image);
-    let url = 'http://localhost:8080/feed/post';
+    let url = process.env.REACT_APP_API_URL + '/feed/post';
     let method = 'POST';
     if (this.state.editPost) {
-      url = 'http://localhost:8080/feed/post/' + this.state.editPost._id;
+      url = process.env.REACT_APP_API_URL + '/feed/post/' + this.state.editPost._id;
       method = 'PUT';
     }
 
@@ -228,7 +228,7 @@ class Feed extends Component {
 
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
-    fetch('http://localhost:8080/feed/post/' + postId, {
+    fetch(process.env.REACT_APP_API_URL + '/feed/post/' + postId, {
       method: 'DELETE',
       headers: {
         'Authorization': 'Bearer ' + this.props.token
